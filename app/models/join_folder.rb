391fd352a0af39363a954285785dfe5561cf0c1d
Onaracs <brendan.scarano@gmail.com>
2015-06-12 07:32:46 -0400

    began adding psuedo element to friends section of navbar

---
+++ app/models/join_folder.rb
@@ -0,0 +1,18 @@
+=begin
+
+1.) Create new model/migration for joining baskets
+- Will belong to user
+- Will belong to folder
+
+2.) When a user clicks on join basket
+- http request to create a new join_basket with
+  - user_id
+  - folder_id
+
+3.) When a user has their baskets returned
+  - user.folders.order('created_at ASC')
+  - ADD!!!
+  - user.join_folders.folders and push to @folders
+
+
+=end
