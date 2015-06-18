ab1d1ae5d39c91912f72e8aae9a632ac6fbcd174
Onaracs <brendan.scarano@gmail.com>
2015-06-18 18:13:43 -0400

    logic in html for displaying followers

--- app/assets/javascripts/ng-app/components/basket-followers-section/basket-followers-section-directive.js
+++ app/assets/javascripts/ng-app/components/basket-followers-section/basket-followers-section-directive.js
@@ -4,15 +4,9 @@ angular.module('basketFollowersSection', [])
     return {
       restrict: 'E',
       scope: {
-        showJoinModal: '='
+        followers: '='
       },
       templateUrl: 'ng-app/components/basket-followers-section/basket-followers-section.html',
-      link: function( scope, $ele, $attrs ) {
-
-        // see if user is already joined here!
-
-      } // link
-
     } // return
 
   })
