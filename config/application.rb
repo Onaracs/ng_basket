6d4feb92a0da0d7b6f2627d1ea2d012193506cd3
Onaracs <brendan.scarano@gmail.com>
2015-06-25 06:59:22 -0400

    changed config file to account for icomoon location

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Basket
     # -- all .rb files in that directory are automatically loaded.
 
     # config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "icomoon")
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
