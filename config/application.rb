fb15ebf11b98839f83de89203a48863a0e32f9a9
Onaracs <brendan.scarano@gmail.com>
2015-06-28 19:28:49 -0400

    working on fixing icons in production

--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,9 @@ module Basket
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
     # config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     config.assets.paths << Rails.root.join("app", "assets", "icomoon")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
