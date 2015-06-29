68f31c85299582a687cdd5f1600fcd1637f51701
Onaracs <brendan.scarano@gmail.com>
2015-06-28 20:35:01 -0400

    added new icon to nav bar

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module Basket
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
-    config.assets.paths << Rails.root.join("app", "assets", "icomoon")
-    config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Eastern Time (US & Canada)'
